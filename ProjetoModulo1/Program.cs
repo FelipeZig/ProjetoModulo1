ae180b87bf8615cc79a0e284afc0a68d31018d5b
Felipe Zsigmond <fe_zig@hotmail.com>
2020-03-02 20:23:28 -0300

    Add project files.

---
+++ ProjetoModulo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoModulo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
