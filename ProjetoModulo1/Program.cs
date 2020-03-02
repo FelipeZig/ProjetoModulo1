e520149c220c00f7f99dc72e10fc4752fafae6b5
Felipe Zsigmond <fe_zig@hotmail.com>
2020-03-02 20:24:35 -0300

    inicio

--- ProjetoModulo1/Program.cs
+++ ProjetoModulo1/Program.cs
@@ -6,7 +6,7 @@ namespace ProjetoModulo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
